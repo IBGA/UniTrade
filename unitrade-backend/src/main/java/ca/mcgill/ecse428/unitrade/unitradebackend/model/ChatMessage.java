04684f96348ce70d96e7323f8caf0602b7963d59
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2023-02-12 21:28:34 -0500

    #11-#76 ChatMessage Model

--- unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/model/ChatMessage.java
+++ unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/model/ChatMessage.java
@@ -2,11 +2,56 @@ package ca.mcgill.ecse428.unitrade.unitradebackend.model;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 public class ChatMessage {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private String content;
+    private Date sendDate;
+    @ManyToOne private Person sender;
+    @ManyToOne private Person receiver;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getSendDate() {
+        return sendDate;
+    }
+
+    public void setSendDate(Date sendDate) {
+        this.sendDate = sendDate;
+    }
+
+    public Person getSender() {
+        return sender;
+    }
+
+    public void setSender(Person sender) {
+        this.sender = sender;
+    }
+
+    public Person getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Person receiver) {
+        this.receiver = receiver;
+    }
 }
