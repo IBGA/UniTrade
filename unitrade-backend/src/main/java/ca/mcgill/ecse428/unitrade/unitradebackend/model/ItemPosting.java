c33535c4532b6d0b0c15447033087d90475fa216
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2023-02-12 21:18:25 -0500

    #11-73 Item Posting Model

--- unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/model/ItemPosting.java
+++ unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/model/ItemPosting.java
@@ -3,9 +3,38 @@ package ca.mcgill.ecse428.unitrade.unitradebackend.model;
 import jakarta.persistence.*;
 
 @Entity
-public class ItemPosting {
+public class ItemPosting extends Post{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private boolean isAvailable;
+
+    public Person getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(Person buyer) {
+        this.buyer = buyer;
+    }
+
+    @OneToOne private Person buyer;
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 }
