6d52580862d5128a0a8c73ad2f3c7857c892ecfc
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2023-02-12 21:20:52 -0500

    #11-73 Post Model

--- unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/model/Post.java
+++ unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/model/Post.java
@@ -2,10 +2,75 @@ package ca.mcgill.ecse428.unitrade.unitradebackend.model;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+import java.util.List;
+
 @Entity
 public class Post {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private String title;
+    private String description;
+    private Date datePosted;
+    @ManyToOne private University university;
+    @OneToOne private Person poster;
+    @OneToMany private List<Course> courses;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(Date datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(University university) {
+        this.university = university;
+    }
+
+    public Person getPoster() {
+        return poster;
+    }
+
+    public void setPoster(Person poster) {
+        this.poster = poster;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
 }
