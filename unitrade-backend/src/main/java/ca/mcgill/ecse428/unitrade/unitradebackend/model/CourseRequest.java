e63cc9342c3e61b3400343b5b61f7d3aa9fe9c86
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2023-02-12 21:01:10 -0500

    #11-#77 CourseRequest Model implemented

--- unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/model/CourseRequest.java
+++ unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/model/CourseRequest.java
@@ -4,9 +4,52 @@ import jakarta.persistence.*;
 
 @Entity
 public class CourseRequest {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isApproved() {
+        return isApproved;
+    }
+
+    public void setApproved(boolean approved) {
+        isApproved = approved;
+    }
+
+    public Person getRequester() {
+        return requester;
+    }
+
+    public void setRequester(Person requester) {
+        this.requester = requester;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(University university) {
+        this.university = university;
+    }
+
+    private boolean isApproved;
+    @OneToOne private Person requester;
+    @OneToOne private Course course;
+    @OneToOne private University university;
 }
