77c4d7cdc98a9a65af5f4fe6c4ba3860edd6797b
Alec Tufenkjian <alectufenkjian@gmail.com>
2023-02-22 23:28:09 -0500

    #53 #61 added login and signup pages

--- unitrade-frontend/src/app.jsx
+++ unitrade-frontend/src/app.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { HomePage } from "./pages/HomePage.jsx";
 import { NotFoundPage } from "./pages/NotFoundPage";
+import { LoginPage } from "./pages/LoginPage";
+import { SignupPage } from "./pages/SignupPage";
 import { NavMenu } from './components/NavMenu';
 import { Footer } from './components/Footer';
 
@@ -13,6 +15,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<HomePage />} />
+          <Route path="/login"  element={<LoginPage />} />
+          <Route path="/signup"  element={<SignupPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
--- unitrade-frontend/src/components/Home.jsx
+++ unitrade-frontend/src/components/Home.jsx
@@ -3,28 +3,13 @@ import Container from 'react-bootstrap/Container';
 import styled from "styled-components";
 
 const HomeStyle = styled.div`
-    .home-page {
-        margin-top: 10rem;
-    }
 `;
 
 export function Home() {
     return (
-        <Container>
+        <Container className="home-container">
             <HomeStyle>
-                <div className="home-page">
-                    <Alert variant="success">
-                        <Alert.Heading>Home Page</Alert.Heading>
-                        <hr />
-                        <p>
-                            Hello! This is a react-bootstrap alert component. Please try to use react-bootstrap components as much as possible to make things look consistent and pretty. Use custom elements only when it's absolutely necessary.
-                            Check the documentation to see what they have to offer and check the code to see how they're implemented.
-                        </p>
-                        <p>
-                            As for custom css, I'm using style-components as they're simple and organized.
-                        </p>
-                    </Alert>
-                </div>
+                
             </HomeStyle>
         </Container>
     );
