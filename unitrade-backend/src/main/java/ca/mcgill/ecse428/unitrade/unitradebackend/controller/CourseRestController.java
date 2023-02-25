4201639ff10500c588c7cabf8bc5e7b43566d17f
Simard302 <Simard302@users.noreply.github.com>
2023-02-24 20:12:36 -0500

    #43 Added course service and controller

---
+++ unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/controller/CourseRestController.java
@@ -0,0 +1,83 @@
+package ca.mcgill.ecse428.unitrade.unitradebackend.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse428.unitrade.unitradebackend.dto.Request.CourseRequestDto;
+import ca.mcgill.ecse428.unitrade.unitradebackend.dto.Response.CourseResponseDto;
+import ca.mcgill.ecse428.unitrade.unitradebackend.model.Course;
+import ca.mcgill.ecse428.unitrade.unitradebackend.service.CourseService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@RestController
+@PreAuthorize("hasRole('USER')")
+@ApiResponses(value = {
+    @ApiResponse(responseCode = "201", description = "Course created"),
+    @ApiResponse(responseCode = "400", description = "Invalid input"),
+    @ApiResponse(responseCode = "404", description = "Referenced resource not found"),
+    @ApiResponse(responseCode = "409", description = "Unique constraint violation")
+})
+public class CourseRestController {
+    
+    @Autowired
+    CourseService courseService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("permitAll()")
+    @PostMapping(value = { "/course"})
+    public ResponseEntity<CourseResponseDto> createCourse(@RequestBody CourseRequestDto body) {
+        Course course = courseService.createCourse(
+            body.getTitle(),
+            body.getCodename(),
+            body.getDescription(),
+            body.getUniversityId()
+        );
+        return new ResponseEntity<CourseResponseDto>(CourseResponseDto.createDto(course), HttpStatus.CREATED);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = { "/course/{id}" })
+    public ResponseEntity<CourseResponseDto> getCourse(@PathVariable("id") Long id) {
+        return new ResponseEntity<CourseResponseDto>(
+                CourseResponseDto.createDto(courseService.getCourse(id)), HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = { "/course/{codename}" })
+    public ResponseEntity<CourseResponseDto> getCourseByCodename(@PathVariable("codename") String codename) {
+        return new ResponseEntity<CourseResponseDto>(
+                CourseResponseDto.createDto(courseService.getCourse(codename)), HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = { "/course" })
+    public ResponseEntity<List<CourseResponseDto>> getAllCourses() {
+        List<Course> courses = courseService.getAllCourses();
+        List<CourseResponseDto> courseResponseDtos = new ArrayList<CourseResponseDto>();
+        for (Course course : courses) {
+            courseResponseDtos.add(CourseResponseDto.createDto(course));
+        }
+
+        return new ResponseEntity<List<CourseResponseDto>>(
+                courseResponseDtos, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(value = { "/course/{id}/approve" })
+    public ResponseEntity<CourseResponseDto> approveCourse(@PathVariable("id") Long id) {
+        return new ResponseEntity<CourseResponseDto>(CourseResponseDto.createDto(courseService.approve(id)), HttpStatus.OK);
+    }
+}
---
+++ unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/service/CourseService.java
@@ -0,0 +1,92 @@
+package ca.mcgill.ecse428.unitrade.unitradebackend.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse428.unitrade.unitradebackend.exception.ServiceLayerException;
+import ca.mcgill.ecse428.unitrade.unitradebackend.model.Course;
+import ca.mcgill.ecse428.unitrade.unitradebackend.model.University;
+import ca.mcgill.ecse428.unitrade.unitradebackend.repository.CourseRepository;
+import ca.mcgill.ecse428.unitrade.unitradebackend.repository.CourseRequestRepository;
+import ca.mcgill.ecse428.unitrade.unitradebackend.repository.UniversityRepository;
+
+@Service
+public class CourseService {
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Autowired
+    UniversityRepository universityRepository;
+
+    @Transactional
+    public Course createCourse(
+            String title,
+            String codename,
+            String description,
+            Long universityId) {
+        
+        // Validate input synteax
+        // Validate input syntax (Error -> 400)
+        if (title == null || title.isEmpty()) {
+            throw new ServiceLayerException(HttpStatus.BAD_REQUEST, "Title cannot be null or empty");
+        }
+
+        if (codename == null || codename.isEmpty()) {
+            throw new ServiceLayerException(HttpStatus.BAD_REQUEST, "Codename cannot be null or empty");
+        }
+
+        if (description == null || description.isEmpty()) {
+            throw new ServiceLayerException(HttpStatus.BAD_REQUEST, "Description name cannot be null or empty");
+        }
+
+        Course course = new Course();
+        course.setTitle(title);
+        course.setApproved(false);
+        course.setDescription(description);
+
+        University university = universityRepository.findById(universityId).orElse(null);
+        if (university == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such university");
+        course.setUniversity(university);
+        return courseRepository.save(course);
+
+    }
+
+    @Transactional
+    public Course getCourse(Long id) {
+        if (id == null) throw new ServiceLayerException(HttpStatus.BAD_REQUEST, "Id cannot be null");
+
+        Course course = courseRepository.findById(id).orElse(null);
+        if (course == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such course");
+        return course;
+    }
+
+    @Transactional
+    public Course getCourse(String codename) {
+        if (codename == null || codename.isEmpty()) throw new ServiceLayerException(HttpStatus.BAD_REQUEST, "Codename cannot be null");
+
+        Course course = courseRepository.findByCodename(codename);
+        if (course == null) throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such course");
+        return course;
+    }
+
+    @Transactional
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    @Transactional
+    public Course approve(Long id){
+        Course course = courseRepository.findById(id).orElse(null);
+
+        if (course == null) {
+            throw new ServiceLayerException(HttpStatus.NOT_FOUND, "No such course");
+        }
+
+        course.setApproved(true);
+        return courseRepository.save(course);
+    }
+}
