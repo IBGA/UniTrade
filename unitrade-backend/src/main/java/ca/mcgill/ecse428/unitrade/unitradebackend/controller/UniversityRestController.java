9724cf7256cb6b3d7bb5c4d4624e51d7d82fa457
Joseph <josephsaliba54@gmail.com>
2023-02-26 16:53:17 -0500

    #45 Uncommented security

--- unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/controller/UniversityRestController.java
+++ unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/controller/UniversityRestController.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
