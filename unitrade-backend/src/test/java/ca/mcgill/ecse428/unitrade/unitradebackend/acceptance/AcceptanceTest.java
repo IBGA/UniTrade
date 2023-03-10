67637971cfb31710213316c034aa8327c4364694
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2023-03-10 15:10:19 -0500

    #124 made it defined port instead of random port

--- unitrade-backend/src/test/java/ca/mcgill/ecse428/unitrade/unitradebackend/acceptance/AcceptanceTest.java
+++ unitrade-backend/src/test/java/ca/mcgill/ecse428/unitrade/unitradebackend/acceptance/AcceptanceTest.java
@@ -4,7 +4,7 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AcceptanceTest {
 
 }
