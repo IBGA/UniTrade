21eebef8ec2bd146cdafd838e2bb14dba3284f01
Joseph <josephsaliba54@gmail.com>
2023-03-19 04:11:46 -0400

    useless commit

--- unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/repository/PersonRepository.java
+++ unitrade-backend/src/main/java/ca/mcgill/ecse428/unitrade/unitradebackend/repository/PersonRepository.java
@@ -2,8 +2,6 @@ package ca.mcgill.ecse428.unitrade.unitradebackend.repository;
 
 import ca.mcgill.ecse428.unitrade.unitradebackend.model.Person;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long>{
