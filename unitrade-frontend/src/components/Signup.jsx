e47bfd835450679a2d2b67c7a0bbb47dae5da12f
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2023-03-19 00:46:19 -0400

    WIP: work on the register page

--- unitrade-frontend/src/components/Signup.jsx
+++ unitrade-frontend/src/components/Signup.jsx
@@ -3,6 +3,8 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import styled from "styled-components";
+import { useState } from 'react';
+import {get, post} from "../utils/client";
 
 const SignupStyle = styled.div`
     .signup-button {
@@ -33,22 +35,69 @@ const SignupStyle = styled.div`
 `;
 
 export function Signup() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [username, setUsername] = useState('');
+
+    const handleChangeFirstName = (e) => setFirstName(e.target.value);
+    const handleChangeLastName = (e) => setLastName(e.target.value);
+    const handleChangeEmail = (e) => setEmail(e.target.value);
+    const handleChangePassword = (e) => setPassword(e.target.value);
+    const handleChangeUsername = (e) => setUsername(e.target.value);
+    const handleChangeConfirmPassword = (e) => setConfirmPassword(e.target.value);
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+
+        let result = await post('person', {firstName, lastName, email, password, username});
+
+        console.log(result);
+    }
     return (
         <SignupStyle>
             <Container className='signup-container'>
                 <Card border="light">
                     <Card.Body>
                         <Card.Title className="text-center signup-title"><b>Create Account</b></Card.Title>
-                        <Form>
+                        <Form onSubmit={submitForm}>
+                            <Form.Group className="mb-3" controlId="formBasicFirstName">
+                                <Form.Label>First Name</Form.Label>
+                                <Form.Control type="text" placeholder="First Name" onChange={handleChangeFirstName} />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicLastName">
+                                <Form.Label>Last Name</Form.Label>
+                                <Form.Control type="text" placeholder="Last Name" onChange={handleChangeLastName} />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicUsername">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="text" placeholder="Last Name" onChange={handleChangeUsername} />
+                            </Form.Group>
+
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Control type="email" placeholder="Enter email" onChange={handleChangeEmail}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control type="password" placeholder="Password" onChange={handleChangePassword}/>
                             </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                                <Form.Label>Confirm Password</Form.Label>
+                                <Form.Control type="password" placeholder="Confirm Password" onChange={handleChangeConfirmPassword} />
+                            </Form.Group>
+
                             <Button variant="dark" type="submit" className='signup-button'>
                                 Sign up
                             </Button>
