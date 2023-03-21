2a3f732ce4cbb76cd0e9456fc5a5e89e25c64637
Simard302 <Simard302@users.noreply.github.com>
2023-03-21 00:38:38 -0400

    Initiial changes

--- unitrade-frontend/src/app.jsx
+++ unitrade-frontend/src/app.jsx
@@ -7,6 +7,7 @@ import { SignupPage } from "./pages/SignupPage";
 import { NavMenu } from './components/NavMenu';
 import { CreateUniversity } from './components/CreateUniversity';
 import { Footer } from './components/Footer';
+import { CreateItemPosting } from "./pages/CreateItemPosting";
 import { BrowseItemPostingPage } from "./pages/BrowseItemPostingPage.jsx";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path="/login"  element={<LoginPage />} />
           <Route path="/signup"  element={<SignupPage />} />
           <Route path="/create-university" element={<CreateUniversity />} />
+          <Route path="/create-item-posting" element={<CreateItemPosting />} />
           <Route path="/browse/post/item" element={<BrowseItemPostingPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
